e1bc927e1421f1b678b31b8eb1cd15d8c4adf8b0
Sêneca Brito <limaseneca@gmail.com>
2025-08-31 12:30:43 -0300

    refactor(services): renomear IAutorService e ILivroService

---
+++ Services/Autor/IAutorService.cs
@@ -0,0 +1,15 @@
+using BibliotecaApi.Models;
+using BibliotecaApi.Models.Dto.Autor;
+
+namespace BibliotecaApi.Services.Autor
+{
+    public interface IAutorService
+    {
+        Task<ResponseModel<List<AutorModel>>> ListarAutores();
+        Task<ResponseModel<AutorModel>> BuscarAutorPorId(int idAutor);
+        Task<ResponseModel<AutorModel>> BuscarAutorPorIdLivro(int idLivro);
+        Task<ResponseModel<List<AutorModel>>> CriarAutor(AutorCriacaoDto autorCriacaoDto);
+        Task<ResponseModel<List<AutorModel>>> EditarAutor(AutorEdicaoDto autorEdicaoDto);
+        Task<ResponseModel<List<AutorModel>>> ExcluirAutor(int idAutor);
+    }
+}
---
+++ Services/Livro/ILivroService.cs
@@ -0,0 +1,15 @@
+using BibliotecaApi.Models;
+using BibliotecaApi.Models.Dto.Livro;
+
+namespace BibliotecaApi.Services.Livro
+{
+    public interface ILivroService
+    {
+        Task<ResponseModel<List<LivroModel>>> ListarLivros();
+        Task<ResponseModel<LivroModel>> BuscarLivroPorId(int idLivro);
+        Task<ResponseModel<List<LivroModel>>> BuscarLivrosPorIdAutor(int idAutor);
+        Task<ResponseModel<List<LivroModel>>> CriarLivro(LivroCriacaoDto livroCriacaoDto);
+        Task<ResponseModel<List<LivroModel>>> EditarLivro(LivroEdicaoDto livroEdicaoDto);
+        Task<ResponseModel<List<LivroModel>>> ExcluirLivro(int idLivro);
+    }
+}
